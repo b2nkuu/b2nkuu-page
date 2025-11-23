74db06774e06960ab3db0e6285f9ed7d207c50c5
B2NKUU <ban.kuu@yahoo.com>
2025-11-24 00:07:27 +0700

    Publish: update files at 2025-11-23 17:07

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "452953170"
+    serviceWorkerVersion: "3918122256"
   }
 });
